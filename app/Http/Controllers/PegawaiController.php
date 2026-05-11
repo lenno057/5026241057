c851d6498874e77c0c8846dbbdb8e991220b57e8
lenno057 <5026241057@student.its.ac.id>
2026-05-11 10:14:02 +0700

    pertemuan 8

---
+++ app/Http/Controllers/PegawaiController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PegawaiController extends Controller{
+    //
+    public function index($nama){
+        return $nama;
+    }
+
+    public function formulir(){
+        return view('formulir');
+    }
+
+    public function proses(Request $request){
+        $nama = $request->input('nama');
+        $umur = $request->input('umur');
+        $alamat = $request->input('alamat');
+        return "Nama : ".$nama.",<br> Umur : ".$umur.",<br> Alamat : ".$alamat;
+    }
+}
